1520ff70dc510d7e8e01d9310f66f84b00dbde71
VickoT <vicko.tornblom@gmail.com>
2025-06-03 12:43:09 +0200

    set up postgres db inside container and add processed data

--- scripts/parse_eggnog.py
+++ scripts/parse_eggnog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
 import pandas as pd
+from sqlalchemy import create_engine
 
 
 class EggNOGAnnotationParser: 
@@ -14,31 +14,38 @@ class EggNOGAnnotationParser:
         """Load EggNOG annotations from a TSV file."""
 
         self.df = pd.read_csv(self.filepath, sep='\t', skiprows=4)
+
+        # Make colnames are consistent with the schema format
+        self.df = self.df.rename(columns={"#query": "query_id"})
+        self.df.columns = [col.lower() for col in self.df.columns]
+
         print(f"Loaded {len(self.df)} EggNOG annotations from {self.filepath}")
         print(self.df)
 
-    def parse_annotations(self):
-        print(self.df.columns)
-        #print(self.df[["GOs"]])
+    def parse_annotations(self): print(self.df.columns)
 
-        columns_to_explode = ["eggNOG_OGs",
-                              "GOs",
-                              "COG_category",
-                              "KEGG_ko",
-                              "KEGG_Pathway",
-                              "BRITE",
-                              "PFAMs"]
+        columns_to_explode = [
+            "eggnog_ogs",
+            "gos",
+            "cog_category",
+            "kegg_ko",
+            "kegg_pathway",
+            "brite",
+            "pfams"
+        ]
 
-        df_long = self.df
+        df_long = self.df.copy()
 
-        for column in df_long.columns:
-            df_long[column] = df_long[column].replace("-", pd.NA)
-            if column in columns_to_explode:
+        # Replace "-" with NaN globally
+        df_long.replace("-", pd.NA, inplace=True)
+
+        # Explode only the relevant columns
+        for column in columns_to_explode:
+            if column in df_long.columns:
                 df_long[column] = df_long[column].str.split(",")
                 df_long = df_long.explode(column)
 
         self.df = df_long
-
         print(df_long)
 
     def save_annotations(self):
@@ -47,10 +54,16 @@ class EggNOGAnnotationParser:
         self.df.to_csv(output_filepath, sep='\t', index=False)
         print(f"Parsed annotations saved to {output_filepath}")
 
+    def export_to_postgres(self, table_name="eggnog_annotations"):
+        engine = create_engine('postgresql://eggnog:password@localhost:5432/eggnogdb')
+        self.df.to_sql(table_name, engine, if_exists='append', index=False)
+        print(f"Annotations exported to PostgreSQL table '{table_name}'")
+
     def run(self):
         self.load_annotations()
         self.parse_annotations()
         self.save_annotations()
+        self.export_to_postgres()
 
 
 def main():
