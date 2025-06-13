960ab2d4893cbbe38b8c2b795cd11db5c3e3d574
VickoT <vicko.tornblom@gmail.com>
2025-06-13 10:43:03 +0200

    Update pytest file

--- tests/test_parse_eggnog.py
+++ tests/test_parse_eggnog.py
@@ -1,30 +1,10 @@
 import pandas as pd
 from scripts.parse_eggnog import EggNOGAnnotationParser
 
-def test_parse_annotations_explodes_columns():
+
+def test_load_raw_eggnog_annotation_data():
     parser = EggNOGAnnotationParser("data/test/dummy_eggnog_annotations.tsv")
     parser.load_annotations()
-    parser.parse_annotations()
-
-    assert not parser.df["gos"].str.contains(",").any() 
-
-
-def test_all_columns_are_atomic():
-    parser = EggNOGAnnotationParser("data/test/dummy_eggnog_annotations.tsv")
-    parser.load_annotations()
-    parser.parse_annotations()
-
-    exploded_columns = [
-        "eggnog_ogs",
-        "gos",
-        "cog_category",
-        "kegg_ko",
-        "kegg_pathway",
-        "brite",
-        "pfams"
-    ]
-
     
-    for col in exploded_columns:
-        if parser.df[col].dtype == object:
-            assert not parser.df[col].astype(str).str.contains(",").any(), f"{col} contains non-atomic values"
+    assert parser.df is not None
+    assert len(parser.df) == 9
